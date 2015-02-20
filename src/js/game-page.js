ee5cf25bff658ee0a51746167a4c057efda944a9
Chris Davies <chris.davies@theironyard.com>
2015-02-20 11:58:38 -0500

    Adding routing

--- src/js/game-page.js
+++ src/js/game-page.js
@@ -1,3 +1,15 @@
+app.router.add('', function () {
+  app.gamePage(8);
+});
+
+app.router.add('easy', function () {
+  app.gamePage(8);
+});
+
+app.router.add('hard', function () {
+  app.gamePage(16);
+});
+
 app.gamePage = function (numPairs) {
   var template = _.template($('#gameBoard').html(), { variable: 'm' });
   var html = template({
--- src/js/init.js
+++ src/js/init.js
@@ -1,1 +1,15 @@
-app.gamePage(8);
+(function () {
+
+  // Hash-based routing
+  function processHash() {
+    var hash = location.hash || '#';
+    if (!app.router.run(hash.substr(1))) {
+      // TODO: Show a 404 page...
+      alert('Could not find page: ' + hash);
+    }
+  }
+
+  window.addEventListener('hashchange', processHash);
+  processHash();
+
+})();
